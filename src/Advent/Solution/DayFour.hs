d741dbd28fb42bd38ff2c4d7387762643c966e3e
Emil Gedda <emil.gedda@emilgedda.se>
2020-12-04 16:16:30 +0100

    simplify

--- src/Advent/Solution/DayFour.hs
+++ src/Advent/Solution/DayFour.hs
@@ -1,15 +1,15 @@
 module Advent.Solution.DayFour (day4) where
 
-import Advent.Problem   (Day, day, Parseable(..), count, between)
-import Control.Arrow    (second)
+import Advent.Problem           (Day, day, Parseable(..), count, between)
+import Control.Arrow            (second)
+import Data.List                ((\\))
+import Data.List.Split          (splitOn)
 import qualified Data.Map as M
-import Data.List        ((\\))
-import Data.List.Split  (splitOn)
 
+newtype Passports = Passports [Passport]
 newtype Passport = Passport {
                         fromPassport :: [(String, String)]
                     }
-newtype Passports = Passports [Passport]
 
 instance Parseable Passport where
     parseString = Passport . map (second (drop 1) . break (==':')) . words
@@ -29,13 +29,13 @@ validate (k, v) = maybe True ($ v) $ validators M.!? k
 validators :: M.Map String (String -> Bool)
 validators =
         let
-            year lower upper = ((&&) . (4 ==) . length) <*> between lower upper
+            year lower upper y  = length y == 4 && between lower upper y
             height (a:b:c:"cm") = between "150" "193" [a,b,c]
             height (x:y:"in")   = between "59" "76" [x,y]
             height _            = False
             hair ('#':color) = length color == 6 && all (`elem` "0123456789abcdef") color
             hair _           = False
-            eye = flip elem ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+            eye   = flip elem ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
             pid n = length n == 9 && all (`elem` "0123456789") n
 
         in M.fromList [
