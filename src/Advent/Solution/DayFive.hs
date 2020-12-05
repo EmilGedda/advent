a6e2e8c4d639d6edf9a570552f6450b843158b2c
Emil Gedda <emil.gedda@emilgedda.se>
2020-12-05 14:55:27 +0100

    simplify bit math

--- src/Advent/Solution/DayFive.hs
+++ src/Advent/Solution/DayFive.hs
@@ -1,14 +1,13 @@
 module Advent.Solution.DayFive (day5) where
 
 import Advent.Problem   (day, Parseable, Day, parseString, both)
-import Data.Bits        ((.|.), shiftL)
-import Data.List        (foldl', sort, (\\))
+import Data.List        (foldl1', sort, (\\))
 
 newtype SeatID = SeatID { fromSeatID :: Int } deriving (Eq, Ord)
 
 instance Parseable SeatID where
-    parseString = SeatID . uncurry ((+) . (8*)) . both (foldl' f 0) . splitAt 7 . map (`elem` "BR")
-        where f acc bit = shiftL acc 1 .|. fromEnum bit
+    parseString = SeatID . uncurry ((+) . (8*)) . both (foldl1' ((+) . (2*)))
+                . splitAt 7 . map (fromEnum . (`elem` "BR"))
 
 day5 :: Day
 day5 = day 5 (maximum . map fromSeatID) (missing . sort . map fromSeatID)
