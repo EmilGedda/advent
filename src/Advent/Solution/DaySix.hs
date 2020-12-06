1c2b8ae08a1f274072a993f51c7a090b1ab42c44
Emil Gedda <emil.gedda@emilgedda.se>
2020-12-06 16:53:12 +0100

    fold intersect instead of filter . group . sort

--- src/Advent/Solution/DaySix.hs
+++ src/Advent/Solution/DaySix.hs
@@ -1,8 +1,8 @@
 module Advent.Solution.DaySix (day6) where
 
 import Advent.Problem               (Parseable, day, Day, parseString)
-import Data.List                    (group, sort)
 import Data.Containers.ListUtils    (nubOrd)
+import Data.List                    (intersect)
 import Data.List.Split              (splitOn)
 
 newtype Answers = Answers { fromAnswers :: [[String]] }
@@ -11,10 +11,7 @@ instance Parseable Answers where
     parseString = Answers . map words . splitOn "\n\n"
 
 day6 :: Day
-day6 = day 6 (collect $ nubOrd . concat) (collect correct)
+day6 = day 6 (collect $ nubOrd . concat) (collect $ foldl1 intersect)
 
 collect :: Foldable t => ([String] -> t a) -> Answers -> Int
 collect f = sum . map (length . f) . fromAnswers
-
-correct :: [String] -> [String]
-correct xs = filter ((== length xs) . length) . group . sort $ concat xs
