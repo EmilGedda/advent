873b20c5ea7ad26945edadb66322c86eea24cb02
Emil Gedda <emil.gedda@emilgedda.se>
2020-12-11 00:04:15 +0100

    small cleanup

--- src/Advent/Solution/DayTen.hs
+++ src/Advent/Solution/DayTen.hs
@@ -1,23 +1,22 @@
 module Advent.Solution.DayTen (day10) where
 
 import           Advent.Problem               (Day, day, count)
-import           Control.Arrow                ((&&&))
 import           Data.List                    (group)
+import           Data.Vector.Algorithms.Intro (sort)
 import qualified Data.Vector                  as V
-import           Data.Vector.Algorithms.Intro as A
 
 day10 :: Day
 day10 = day 10 (partOne . diffs) (partTwo . diffs)
 
 partOne :: V.Vector Int -> Int
-partOne = uncurry (*) . (count (==1) &&& count (==3))
+partOne = (*) <$> count (==1) <*> count (==3)
 
 diffs :: V.Vector Int -> V.Vector Int
-diffs = flip V.snoc 3 . (V.zipWith (-) =<< V.drop 1) . V.cons 0 . V.modify A.sort
+diffs = flip V.snoc 3 . (V.zipWith (-) =<< V.drop 1) . V.cons 0 . V.modify sort
 
 partTwo :: V.Vector Int -> Int
-partTwo = product . map (combinations . length) . filter (all (==1)) . group . V.toList
+partTwo = product . map (tribonacci . length) . filter (all (==1)) . group . V.toList
 
--- Tribonacci sequence specialized to a closed form formula for given the input
-combinations :: Int -> Int
-combinations n = (n ^ 2 - n) `div` 2 + 1
+-- Tribonacci sequence as a closed form formula only valid for given the input
+tribonacci :: Int -> Int
+tribonacci n = (n ^ 2 - n) `div` 2 + 1
