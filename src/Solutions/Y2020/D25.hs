a44b1d0e39f50a127d3116a00db44ea860341478
Emil Gedda <emil.gedda@emilgedda.se>
2021-02-09 20:21:20 +0100

    use binary rtl modexp

--- src/Solutions/Y2020/D25.hs
+++ src/Solutions/Y2020/D25.hs
@@ -2,20 +2,24 @@
 module Solutions.Y2020.D25 (day25) where
 
 import Advent.Problem
+import Data.Bits
 
 day25 :: Day 25
 day25 = day encKey notSolved
 
 encKey :: [Int] -> Int
-encKey [card, door] = modpow door (loopSize card)
+encKey [card, door] = modexp door (loopSize card)
 
-loopSize :: Int -> Int
-loopSize n = length
-           . takeWhile (/=n)
-           $ iterate (flip mod 20201227 . (*7)) 1
+loopSize :: Integral a => a -> Int
+loopSize c = length . takeWhile (/=c) $ iterate (transform 7) 1
 
-modpow :: Int -> Int -> Int
-modpow b e = go e 1
-    where f n = n * b `mod` 20201227
-          go 0 x = x
-          go n !x = go (n - 1) (f x)
+modexp :: Int -> Int -> Int
+modexp b e = go e 1 b
+    where go 0 res _ = res
+          go exp res base =
+              let res' | odd exp = transform res base
+                       | otherwise = res
+              in go (exp `shiftR` 1) res' (transform base base)
+
+transform :: Integral a => a -> a -> a
+transform b e = b * e `mod` 20201227
