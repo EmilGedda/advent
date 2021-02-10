be4545be59e8a6d92e4081d0420eb00870f82d97
Emil Gedda <emil.gedda@emilgedda.se>
2021-02-10 22:01:30 +0100

    use baby-step giant-step in dlp

--- src/Solutions/Y2020/D25.hs
+++ src/Solutions/Y2020/D25.hs
@@ -1,25 +1,30 @@
-{-# LANGUAGE BangPatterns #-}
 module Solutions.Y2020.D25 (day25) where
 
 import Advent.Problem
 import Data.Bits
+import Data.Maybe (catMaybes)
+import qualified Data.IntMap as M
 
 day25 :: Day 25
 day25 = day encKey notSolved
 
 encKey :: [Int] -> Int
-encKey [card, door] = modexp door (loopSize card)
+encKey [card, door] = modExp door (discreteLog card)
 
-loopSize :: Integral a => a -> Int
-loopSize c = length . takeWhile (/=c) $ iterate (transform 7) 1
+discreteLog :: Int -> Int
+discreteLog = head . catMaybes . zipWith calc [0..] . iterate (transform factor)
+    where table    = M.fromList . flip zip [0..modulo] $ iterate (transform 7) 1
+          calc i e = (modulo * i +) <$> table M.!? e
+          factor   = modExp 7 $ 20201227 - modulo - 1
+          modulo   = ceiling  $ sqrt 20201227
 
-modexp :: Int -> Int -> Int
-modexp b e = go e 1 b
-    where go 0 res _ = res
+modExp :: Int -> Int -> Int
+modExp b e = go e 1 b
+    where go 0   res _    = res
           go exp res base =
-              let res' | odd exp = transform res base
+              let res' | odd exp   = transform res base
                        | otherwise = res
               in go (exp `shiftR` 1) res' (transform base base)
 
 transform :: Integral a => a -> a -> a
-transform b e = b * e `mod` 20201227
+transform b e = b * e `rem` 20201227
