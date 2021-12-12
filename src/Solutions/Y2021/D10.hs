dcd25a67cd615dbb1e41cd33eab3cc21ea033d8a
Emil Gedda <emil.gedda@emilgedda.se>
2021-12-12 13:34:01 +0100

    cleanup

--- src/Solutions/Y2021/D10.hs
+++ src/Solutions/Y2021/D10.hs
@@ -12,9 +12,9 @@ syntaxError :: [Either a Char] -> Int
 syntaxError = sum . map corruptScore . rights
 
 autocompete :: [Either String a] -> Int
-autocompete lines =
-    let score = map (foldl' (\acc n -> acc * 5 + completionScore n) 0) $ lefts lines
-     in sort score !! (length score `div` 2)
+autocompete lines = sort score !! (length score `div` 2)
+    where score = map (foldl' (\acc n -> acc * 5 + completionScore n) 0)
+                $ lefts lines
 
 corruptScore :: Char -> Int
 corruptScore = \case
@@ -33,13 +33,11 @@ completionScore = \case
 parse :: String -> Either String Char
 parse = go []
     where
-        opens = flip elem "([{<"
-
         go leftover [] = Left leftover
         go ('(':stack) (')':rest) = go stack rest
         go ('[':stack) (']':rest) = go stack rest
         go ('{':stack) ('}':rest) = go stack rest
         go ('<':stack) ('>':rest) = go stack rest
         go stack (next:rest)
-            | opens next = go (next:stack) rest
+            | next `elem` "([{<" = go (next:stack) rest
             | otherwise = Right next
